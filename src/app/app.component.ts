3d8a5a92c0b01f8d3b9d6068a02785fc0afe6498
DegalaJabeer <81851270+DegalaJabeer@users.noreply.github.com>
2022-07-19 20:05:25 +0530

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'candidate-form';
+  onSubmit(contactForm:any) {
+    console.log(contactForm.value);
+  }
+}
+
+
